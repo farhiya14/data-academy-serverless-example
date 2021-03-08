9ca5b7fa229f3db34cdcdd28d6ea96593c86e3a6
Lewis Richardson <lewisjakerichardson@hotmail.com>
2021-03-08 13:14:03 +0000

    update serverless and handler

--- src/example-service-1/handler.py
+++ src/example-service-1/handler.py
@@ -1,15 +1,14 @@
-import json
 import boto3
+import csv
 
 def handle(event):
-    body = {
-        "message": "Your function executed for service 2!",
-        "input": event
-    }
-
-    response = {
-        "statusCode": 200,
-        "body": json.dumps(body)
-    }
-
-    return response
+    # List all files in bucket
+    key = event['Records'][0]['s3']['object']['key']
+    bucket = event['Records'][0]['s3']['bucket']['name']
+    
+    # get_csv_data_from_bucket
+    s3 = boto3.client('s3')
+    s3_object = s3.get_object(Bucket = bucket, Key = key)
+    data = s3_object['Body'].read().decode('utf-8')
+    csv_data = csv.reader(data.splitlines())
+    print(csv_data)
